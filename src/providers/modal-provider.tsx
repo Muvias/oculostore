5170f58826e8e78d0b741709685e12c5c55f7a89
Vinicius Perazza <vi_guepardo@hotmail.com>
2024-06-17 17:04:23 -0300

    first commit

---
+++ src/providers/modal-provider.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { StoreModal } from "@/components/modals/store-modal";
+import { useEffect, useState } from "react"
+
+export function ModalProvider() {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    if (!isMounted) null;
+
+    return (
+        <>
+            <StoreModal />
+        </>
+    )
+}
